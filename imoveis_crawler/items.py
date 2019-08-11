0d3dd39c7f76ef684988e50c18a1c7870a742ae7
Gabriel Vitor Campos <gabrielcampos.contato@gmail.com>
2019-08-10 23:11:48 -0300

    primeiro commit

---
+++ imoveis_crawler/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ImoveisCrawlerItem(scrapy.Item):
+    preco = scrapy.Field()
+    area = scrapy.Field()
+    quartos = scrapy.Field()
+    banheiros = scrapy.Field()
+    vagas_garagem = scrapy.Field()
+    condominio = scrapy.Field()
+    iptu = scrapy.Field()
+    endereco = scrapy.Field()
+    imobiliaria = scrapy.Field()
+
---
+++ imoveis_crawler/spiders/zapimoveis.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class ZapimoveisSpider(CrawlSpider):
+    name = 'zapimoveis'
+    allowed_domains = ['www.zapimoveis.com.br']
+    start_urls = ['https://www.zapimoveis.com.br/venda/imoveis/']
+    user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
+
+    rules = (
+        Rule(LinkExtractor(allow="/imovel/venda")),
+        Rule(LinkExtractor(allow="/"), callback="parse_zap")
+    )
+
+    def parse_zap(self, response):
+        print("IMOVEL")
